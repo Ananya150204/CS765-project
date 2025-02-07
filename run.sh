c6238874dec661a678265b19fbd287e2267b42a6
Ananya <ananyakulashreshtha@gmail.com>
2025-02-07 23:44:30 +0530

    Faltu

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 make clean
 make
-./simulator --num_peers 20 --slow_percent 80 --low_cpu_percent 20 --transaction_mean_time 5000000 --block_mean_time 28000000 --end_time 1200000000 > out.txt
+./simulator --peers 50 --slow_percent 100 --low_cpu_percent 100 --txn_interarrival 10000000 --block_interarrival_time 600000000 --end_time 12000000000 > out.txt
 python3 src/visualize.py
 
 
