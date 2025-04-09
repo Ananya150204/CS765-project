6e42b54e454fb8dedebd3693feb8a4b89ab12692
Tanish1e9 <tanish.ag1295@gmail.com>
2025-04-09 15:29:51 +0530

    Fixed some fees error...

--- Assgn3/DEX.sol
+++ Assgn3/DEX.sol
@@ -11,19 +11,21 @@ contract DEX {
     IERC20 public tokenB;
     LPToken public lpToken;
 
+    uint256 internal reserveA;
+    uint256 internal reserveB;
+
     constructor(address _tokA, address _tokB) {
         tokenA = IERC20(_tokA);
         tokenB = IERC20(_tokB);
         lpToken = new LPToken(address(this));
+        reserveA = 0;
+        reserveB = 0;
     }
 
     function addLiquidity(uint256 amountA, uint256 amountB) public {
         require(amountA > 0 && amountB > 0, "Zero amounts");
         require(tokenA.balanceOf(msg.sender) >= amountA, "Insufficient token A balance");
         require(tokenB.balanceOf(msg.sender) >= amountB, "Insufficient token B balance");
-
-        uint reserveA = tokenA.balanceOf(address(this));
-        uint reserveB = tokenB.balanceOf(address(this));
 
         if (reserveA == 0 && reserveB == 0) {
             // First LP sets the ratio
@@ -46,6 +48,9 @@ contract DEX {
             tokenA.transferFrom(msg.sender, address(this), amountA);
             tokenB.transferFrom(msg.sender, address(this), amountB);
         }
+
+        reserveA += amountA;
+        reserveB += amountB;
     }
 
     function removeLiquidity(uint256 lpAmount) public  {
@@ -54,9 +59,6 @@ contract DEX {
         require(totalSupply > 0, "No LP tokens");
         require(lpToken.balanceOf(msg.sender) >= lpAmount, "Insufficient LP balance");
 
-        uint reserveA = tokenA.balanceOf(address(this));
-        uint reserveB = tokenB.balanceOf(address(this));
-
         uint256 amountA = (lpAmount * reserveA) / totalSupply;
         uint256 amountB = (lpAmount * reserveB) / totalSupply;
 
@@ -64,59 +66,56 @@ contract DEX {
 
         tokenA.transfer(msg.sender, amountA);
         tokenB.transfer(msg.sender, amountB);
+
+        reserveA -= amountA;
+        reserveB -= amountB;
     }
 
     function swap_A_to_B(uint256 amt) public {
         require(tokenA.balanceOf(msg.sender) >= amt, "Insufficient token A balance");
-        uint reserveA = tokenA.balanceOf(address(this));
-        uint reserveB = tokenB.balanceOf(address(this));
 
         uint256 eff_A = (997*amt)/1000;        
         uint256 amt_B = (eff_A * reserveB) / (reserveA + eff_A);
         require(tokenB.balanceOf(address(this)) >= amt_B, "Insufficient token B reserves");
 
+        tokenA.transferFrom(msg.sender, address(this), amt);
         tokenB.transfer(msg.sender, amt_B);
-        tokenA.transferFrom(msg.sender, address(this), eff_A);
+
+        reserveA += eff_A;
+        reserveB -= amt_B;
     }
 
     function swap_B_to_A(uint amt) public {
         require(tokenB.balanceOf(msg.sender) >= amt, "Insufficient token B balance");
-        uint reserveA = tokenA.balanceOf(address(this));
-        uint reserveB = tokenB.balanceOf(address(this));
 
         uint256 eff_B = (997*amt)/1000;        
         uint256 amt_A = (eff_B * reserveA) / (reserveB + eff_B);
         require(tokenA.balanceOf(address(this)) >= amt_A, "Insufficient token A reserves");
 
+        tokenB.transferFrom(msg.sender, address(this), amt);
         tokenA.transfer(msg.sender, amt_A);
-        tokenB.transferFrom(msg.sender, address(this), eff_B);
+
+        reserveA -= amt_A;
+        reserveB += eff_B;
     }
 
     function get_B_to_A(uint256 amt_B) public view returns (uint256){
-        uint reserveA = tokenA.balanceOf(address(this));
-        uint reserveB = tokenB.balanceOf(address(this));
         return (reserveA * amt_B)/reserveB;
     }
 
     function get_A_to_B(uint256 amt_A) public view returns (uint256){
-        uint reserveA = tokenA.balanceOf(address(this));
-        uint reserveB = tokenB.balanceOf(address(this));
         return (reserveB * amt_A)/reserveA;
     }
 
     function get_spot_price() public view returns (uint256){
-        uint reserveA = tokenA.balanceOf(address(this));
-        uint reserveB = tokenB.balanceOf(address(this));
         return reserveA/reserveB;
     }
 
     function get_reserveA() public view returns (uint256){
-        uint reserveA = tokenA.balanceOf(address(this));
         return reserveA;
     } 
 
     function get_reserveB() public view returns (uint256){
-        uint reserveB = tokenB.balanceOf(address(this));
         return reserveB;
     } 
 
@@ -124,3 +123,4 @@ contract DEX {
         return lpToken.balanceOf(msg.sender);
     }
 }
+
